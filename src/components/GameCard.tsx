5514e0f2edf65ea5ae6c6d40d012c1731c1940c1
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-08-11 20:07:48 +0300

    added loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../common.types";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
-import getCroppedImageUrl from './../services/image-url';
+import getCroppedImageUrl from "./../services/image-url";
 
 interface Props {
   game: Game;
@@ -10,11 +10,11 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card borderRadius={10} overflow="hidden" width="300px">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
