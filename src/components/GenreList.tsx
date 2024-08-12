2bc52dc279d73ffa7fe140ec5c4f046d03fda47a
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-08-12 19:36:57 +0300

    added spinner for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,18 +1,26 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
-import getCroppedImageUrl from './../services/image-url';
+import getCroppedImageUrl from "./../services/image-url";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+
+  if (error) null;
+  
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
       {genres.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
+        <ListItem key={genre.id} paddingY="5px">
           <HStack>
-            <Image boxSize='34px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-            <Text fontSize='lg'>{genre.name}</Text>
-            </HStack>
+            <Image
+              boxSize="34px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
         </ListItem>
       ))}
     </List>
