488561f8ad616b8e7e0611194b6dfe5ed00119ad
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-08-20 10:07:49 +0300

    changed genres to a static asset

--- tests/components/GenreList.test.tsx
+++ tests/components/GenreList.test.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import GenreList from "./../../src/components/GenreList";
-import { genres } from "../mocks/data";
-import { server } from "../mocks/server";
-import { http, HttpResponse } from "msw";
+import genres from "../../src/data/genres";
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/vitest'
 
@@ -39,15 +37,6 @@ describe("GenreList", () => {
     });
   });
 
-  it("should not render anything when there is an error", async () => {
-    server.use(
-      http.get("https://api.rawg.io/api/genres", () => HttpResponse.error())
-    );
-
-    const { genreList } = await renderGenreListComponent();
-
-    expect(genreList).toBeEmptyDOMElement();
-  });
 
   it('Should call onSelectGenre when list item is clicked', async() => {
     const {onSelectGenre} = await renderGenreListComponent();
--- tests/hooks/useGenres.test.ts
+++ tests/hooks/useGenres.test.ts
@@ -1,9 +1,6 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import useGenres from "./../../src/hooks/useGenres";
-import { http, HttpResponse } from "msw";
-import { server } from "../mocks/server";
-import { genres } from "../mocks/data";
 
 describe("useGenres", () => {
   it("should return a list of genres", async () => {
@@ -12,22 +9,9 @@ describe("useGenres", () => {
     await waitFor(() => {
       const genreResults = result.current.data;
       expect(genreResults.length).toBeGreaterThan(0);
-      expect(genreResults).toEqual(genres);
+      
     });
   });
 
-  it("should return error when there is an error", async () => {
-    server.use(
-      http.get("https://api.rawg.io/api/genres", () => HttpResponse.error())
-    );
-
-    const { result } = renderHook(() => useGenres());
-
-    // Assert that the initial state is loading
-    expect(result.current.isLoading).toBe(true);
-
-    await waitFor(() => expect(result.current.isLoading).toBe(false));
-
-    expect(result.current.error).not.toBeNull();
-  });
+ 
 });
