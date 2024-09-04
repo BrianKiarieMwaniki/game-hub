f20a77ad2c4fa96f0fb17852b226e53f13bc0eda
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-09-04 19:49:43 +0300

    created a look up hook

--- src/common.types.ts
+++ src/common.types.ts
@@ -6,29 +6,28 @@ export interface GameQuery {
   page?: any;
 }
 
+export interface Identity{
+  id:number;
+  name:string;
+}
+
 export interface FetchResponse<T> {
   count: number;
   results: T[];
   next: string | null;
 }
 
-export interface Game {
-  id: number;
-  name: string;
+export interface Game extends Identity {
   background_image: string;
   metacritic: number;
   parent_platforms: { platform: Platform }[];
   rating_top: number;
 }
 
-export interface Platform {
-  id: number;
+export interface Platform extends Identity{
   slug: string;
-  name: string;
 }
-export interface Genre {
-  id: number;
-  name: string;
+export interface Genre extends Identity {
   slug: string;
   games_count: number;
   image_background: string;
--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,7 +1,6 @@
 import { Heading } from "@chakra-ui/react";
-import { GameQuery } from "../common.types";
-import useGenres from "../hooks/useGenres";
-import usePlatforms from "../hooks/usePlatforms";
+import { GameQuery, Genre, Platform } from "../common.types";
+import { useGenres, useLookup, usePlatforms } from "../hooks";
 
 interface Props {
   gameQuery: GameQuery;
@@ -10,14 +9,18 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const { platformId, genreId } = gameQuery;
 
-  const {data: genres} =useGenres();
-  const {data:platforms} = usePlatforms();
+  const { data: genres } = useGenres();
+  const { data: platforms } = usePlatforms();
 
-  const genreName = genres?.find(genre => genre.id === genreId)?.name;
-  const platformName = platforms?.find(platform => platform.id === platformId)?.name;
+  const { name: genreName } = useLookup<Genre>(genres, genreId);
+  const { name: platformName } = useLookup<Platform>(platforms, platformId);
 
-  const heading = `${platformName || ''} ${genreName || ''} Games`;
-  return <Heading as="h1" marginBottom={5} fontSize='5xl' data-testid='game-heading'>{heading}</Heading>;
+  const heading = `${platformName || ""} ${genreName || ""} Games`;
+  return (
+    <Heading as="h1" marginBottom={5} fontSize="5xl" data-testid="game-heading">
+      {heading}
+    </Heading>
+  );
 };
 
 export default GameHeading;
