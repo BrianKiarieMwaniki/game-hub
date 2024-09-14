809ecf9fc3425a643e6e24bea21a3c723ffd2caa
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-09-14 19:16:22 +0300

    built game trailer

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,5 +2,7 @@ import {Game} from './Game';
 import { Genre } from './Genre';
 import { Platform } from './Platform';
 import { Publisher } from './Publisher';
+import { Trailer } from './Trailer';
 
-export type {Game, Genre, Platform, Publisher};
+
+export type {Game, Genre, Platform, Publisher, Trailer};
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,9 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { useGame } from "../hooks";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import { CriticScore } from "../components";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
+import { useGame } from "../hooks";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -20,6 +19,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText text={game.description_raw} />
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
